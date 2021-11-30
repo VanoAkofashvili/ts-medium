bbe8f409f754f70d573b16c176d51d4d58b4e80e
Vaniko Akopashvili <v.akopashvili@sweeftdigital.com>
2021-11-30 14:51:54 +0400

    Setup store

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
